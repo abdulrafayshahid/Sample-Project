d9638c3f55d6f14c85b159c5fc83c59edf8f1e8e
abdulrafayshahid <abdulrafay99910@gmail.com>
2023-09-11 20:48:21 +0500

    //

--- employee_list.sql
+++ employee_list.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 04, 2023 at 07:01 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Host: localhost:3306
+-- Generation Time: Sep 11, 2023 at 03:47 PM
+-- Server version: 8.0.30
+-- PHP Version: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,18 +24,58 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `qualifications`
+--
+
+CREATE TABLE `qualifications` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `qualifications`
+--
+
+INSERT INTO `qualifications` (`id`, `name`) VALUES
+(1, 'Matric'),
+(2, 'Intermediate'),
+(3, 'BS'),
+(4, 'MS');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
-  `Name` varchar(255) NOT NULL,
-  `Age` int(255) NOT NULL,
-  `id` int(255) NOT NULL
+  `Name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `Age` int NOT NULL,
+  `id` int NOT NULL,
+  `verification` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`Name`, `Age`, `id`, `verification`) VALUES
+('abdul', 23, 355, 'verified'),
+('maaz', 15, 356, 'unverified'),
+('musadiq', 30, 357, 'verified'),
+('amash', 18, 358, 'verified'),
+('raid', 40, 359, 'verified'),
+('shahid', 12, 360, 'unverified');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `qualifications`
+--
+ALTER TABLE `qualifications`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
@@ -48,10 +88,16 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `qualifications`
+--
+ALTER TABLE `qualifications`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=361;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
